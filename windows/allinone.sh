7c791460f091bfddf1d31767c1710b6c4278e52d
Luiz Guilherme S. Munhoz ✨ <94804469+frpitu@users.noreply.github.com>
2024-06-23 19:04:54 -0300

    Create allinone.sh

---
+++ windows/allinone.sh
@@ -0,0 +1,9 @@
+# Instale o Git (se ainda não estiver instalado)
+apt update
+apt install git
+
+# Navegue para o diretório desejado
+cd storage/shared/MeusArquivos
+
+# Clone o repositório do GitHub
+git clone https://github.com/usuario/nome-do-repositorio.git
