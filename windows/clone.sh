982004862f9a6ccab5da48ec65c6aa3e70d11ca5
Luiz Guilherme S. Munhoz ✨ <94804469+frpitu@users.noreply.github.com>
2024-06-23 19:12:57 -0300

    Delete windows/clone.sh

--- windows/clone.sh
+++
@@ -1,5 +0,0 @@
-# Supondo que você queira clonar um repositório para a pasta "Projetos"
-cd C:\Users\SeuUsuario\Projetos
-
-# Clone o repositório do GitHub
-git clone https://github.com/usuario/nome-do-repositorio.git
